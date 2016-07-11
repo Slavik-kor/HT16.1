5db9b0b69131947f2f4aba3ca83f829e614ea42e
Viachaslau Korotki <Viachaslau Korotki>
2016-07-12 00:21:46 +0300

    commit

---
+++ src/port/Berth.java
@@ -0,0 +1,44 @@
+package port;
+
+import warehouse.Warehouse;
+
+public class Berth {
+
+	private int id;
+	private Warehouse portWarehouse;
+
+	public Berth(int id, Warehouse warehouse) {
+		this.id = id;
+		portWarehouse = warehouse;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public boolean add(Warehouse shipWarehouse, int numberOfConteiners) throws InterruptedException {
+		boolean result = false;
+
+		synchronized (portWarehouse) {
+			if (portWarehouse.addContainer(shipWarehouse.getContainer(numberOfConteiners))) {
+				result = true;
+			}
+		}
+
+		return result;
+
+	}
+
+	public boolean get(Warehouse shipWarehouse, int numberOfConteiners) throws InterruptedException {
+		boolean result = false;
+
+		synchronized (portWarehouse) {
+			if (shipWarehouse.addContainer(portWarehouse.getContainer(numberOfConteiners))) {
+				result = true;
+			}
+		}
+
+		return result;
+	}
+
+}
---
+++ src/port/Port.java
@@ -0,0 +1,78 @@
+package port;
+
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import ship.Ship;
+import warehouse.Container;
+import warehouse.Warehouse;
+
+public class Port {
+	private static final Logger LOGGER = LogManager.getLogger();
+	private BlockingQueue<Berth> berthList; // очередь причалов
+	private Warehouse portWarehouse; // хранилище порта
+	private Map<Ship, Berth> usedBerths; // какой корабль у какого причала стоит
+
+	public Port(int berthSize, int warehouseSize) {
+		portWarehouse = new Warehouse(warehouseSize); // создаем пустое
+														// хранилище
+		berthList = new ArrayBlockingQueue<Berth>(berthSize); // создаем очередь причалов
+		for (int i = 0; i < berthSize; i++) { // заполняем очередь причалов
+												// непосредственно самими
+												// причалами
+			berthList.add(new Berth(i, portWarehouse));
+		}
+		usedBerths = new ConcurrentHashMap<Ship, Berth>(); // создаем объект, который
+													// будет
+		LOGGER.warn("Порт создан.");
+	}
+
+	
+	  public void setContainersToWarehouse(List<Container> containerList){
+	  portWarehouse.addContainer(containerList); }
+	 
+
+	public boolean lockBerth(Ship ship) {
+		boolean result = false;
+		Berth berth;
+
+		//!!!!!!!!!
+		//synchronized (berthList) {
+			berth = berthList.poll();
+		//}
+		
+		if (berth != null) {
+			result = true;
+			usedBerths.put(ship, berth);
+		}	
+		
+		return result;
+	}
+
+	public boolean unlockBerth(Ship ship) {
+		Berth berth = usedBerths.get(ship);
+
+		synchronized (berthList) {
+			berthList.offer(berth);
+			usedBerths.remove(ship);	
+		}		
+		
+		return true;
+	}
+
+	public Berth getBerth(Ship ship) throws PortException {
+
+		Berth berth = usedBerths.get(ship);
+		if (berth == null) {
+			throw new PortException("Try to use Berth without blocking.");
+		}
+		return berth;
+	}
+}
